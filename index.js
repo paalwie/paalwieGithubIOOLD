5c6d068ef0d62a307f8dbac4f818f1d241c43005
Patrick Wiedenmann <34657607+paalwie@users.noreply.github.com>
2024-07-12 08:41:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const projekteListe = document.querySelector('.projekte ul');
+
+// GitHub-Benutzername und Repository-Liste ersetzen
+const benutzername = "paalwie";
+const repositories = ["alteIOSeite", "datenbankonline", "Netzplan"]; // Weitere Repos hinzufügen
+
+// Funktion zum Erstellen eines Projektlisteneintrags
+function createProjektEintrag(repoName) {
+    const projektEintrag = document.createElement('li');
+    projektEintrag.classList.add('projekt');
+
+    const projektNameElement = document.createElement('span');
+    projektNameElement.classList.add('projekt-name');
+    projektNameElement.textContent = repoName;
+
+    const projektLinkElement = document.createElement('a');
+    projektLinkElement.classList.add('projekt-link');
+    projektLinkElement.href = `https://github.com/${benutzername}/${repoName}`;
+    projektLinkElement.textContent = `Link zu ${repoName}`;
+
+    projektEintrag.appendChild(projektNameElement);
+    projektEintrag.appendChild(projektLinkElement);
+
+    return projektEintrag;
+}
+
+// Projekte zur Liste hinzufügen
+repositories.forEach(repoName => {
+    const projektEintrag = createProjektEintrag(repoName);
+    projekteListe.appendChild(projektEintrag);
+});
